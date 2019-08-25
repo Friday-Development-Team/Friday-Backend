3a977dc13cc64a6a2613d276a3303bfb0ba21e5a
Nick Lersberghe <nick.lersberghe@student.hogent.be>
2019-08-25 17:01:10 +0200

    First commit. Added Item class.

---
+++ Friday/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Friday.Models {
+    /// <summary>
+    /// Class containing information about Items used in the shop. Contains only basic information, such as name, price, type (food or drink) and the amount left.
+    /// </summary>
+    public class Item {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Type { get; set; }
+        public int Count { get; set; }
+    }
+}
--- Friday/Startup.cs
+++ Friday/Startup.cs
@@ -22,6 +22,8 @@ namespace Friday {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddOpenApiDocument();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +38,9 @@ namespace Friday {
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            app.UseSwaggerUi3();
+            app.UseSwagger();
         }
     }
 }
