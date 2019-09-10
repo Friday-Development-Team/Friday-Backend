728fa3f3816cb79a12d944e4c3c06f829db34ea0
nlersber <unconfigured@null.spigotmc.org>
2019-09-10 13:12:28 +0200

    Configured auth service in startup

--- Friday/Startup.cs
+++ Friday/Startup.cs
@@ -1,8 +1,10 @@
-﻿using Friday.Data;
+﻿using System;
+using Friday.Data;
 using Friday.Data.IServices;
 using Friday.Data.ServiceInstances;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -28,12 +30,35 @@ namespace Friday {
             services.AddDbContext<Context>(Options =>
                 Options.UseSqlServer(Configuration.GetConnectionString("Context")));
 
+            services.AddCors(options => options.AddPolicy("AllowAllOrigins", builder => builder.AllowAnyOrigin()));
+
             services.AddOpenApiDocument(c => {
                 c.DocumentName = "apidocs";
                 c.Title = "Friday API";
                 c.Version = "v1";
                 c.Description = "The Friday API documentation description.";
             }); //for OpenAPI 3.0 else AddSwaggerDocument();
+
+            services.AddIdentity<IdentityUser, IdentityRole>(cfg => cfg.User.RequireUniqueEmail = true).AddEntityFrameworkStores<Context>();
+            services.Configure<IdentityOptions>(options => {
+                // Password settings.
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 0;
+
+                // Lockout settings.//Options are redundant, set values are default
+                //options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                //options.Lockout.MaxFailedAccessAttempts = 5;
+                //options.Lockout.AllowedForNewUsers = true;
+
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                options.User.RequireUniqueEmail = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +73,8 @@ namespace Friday {
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            app.UseCors("AllowAllOrigins");
 
             app.UseSwaggerUi3();
             app.UseSwagger();
