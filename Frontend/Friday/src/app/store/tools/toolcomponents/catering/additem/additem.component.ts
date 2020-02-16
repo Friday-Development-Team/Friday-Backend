786713a599278e836e296d9b544498198fd7f231
Nick Lersberghe <nick.lersberghe@student.hogent.be>
2020-02-16 16:58:42 +0100

    Added AddItem form building

--- Frontend/Friday/src/app/store/tools/toolcomponents/catering/additem/additem.component.ts
+++ Frontend/Friday/src/app/store/tools/toolcomponents/catering/additem/additem.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Item } from 'src/app/models/models';
 import { AdminService } from 'src/app/services/admin.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'friday-additem',
@@ -10,12 +11,32 @@ import { AdminService } from 'src/app/services/admin.service';
 export class AdditemComponent implements OnInit {
 
   item: Item
+  form: FormGroup
 
-  constructor(private admin: AdminService) { }
+  items: string[]
+
+  constructor(private admin: AdminService, fb: FormBuilder) {
+    const decimal = '[0-9]+(\.?[0-9]+)?'
+    const nodecimal = '[1-9]?[0-9]+'
+    this.form = fb.group({
+      name: fb.control('', [Validators.required]),
+      price: fb.control('', [Validators.required, Validators.pattern(decimal)]),
+      type: fb.control('', Validators.required),
+      count: fb.control('', [Validators.required, Validators.pattern(nodecimal)]),
+      image: fb.control('', [Validators.required, Validators.pattern('[a-z0-9]+(_[a-z0-9]+)*')]),
+      details: fb.group({
+        size: fb.control('', [Validators.required]),
+        calories: fb.control('', [Validators.required, Validators.pattern(nodecimal)]),
+        sugarcontent: fb.control('', [Validators.required, Validators.pattern(decimal)]),
+        saltcontent: fb.control('', [Validators.required, Validators.pattern(decimal)]),
+        allergens: fb.control('')
+      })
+    })
+  }
 
   ngOnInit() {
   }
 
-  
+
 
 }
