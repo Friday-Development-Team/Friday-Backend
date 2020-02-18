3482dcb5eed61182619696003a11a2287dbc836d
Nick Lersberghe <nick.lersberghe@student.hogent.be>
2020-02-18 11:36:40 +0100

    Additem

--- Frontend/Friday/src/app/store/tools/toolcomponents/catering/additem/additem.component.ts
+++ Frontend/Friday/src/app/store/tools/toolcomponents/catering/additem/additem.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Item } from 'src/app/models/models';
 import { AdminService } from 'src/app/services/admin.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'friday-additem',
@@ -15,8 +16,8 @@ export class AdditemComponent implements OnInit {
 
   items: string[]
 
-  constructor(private admin: AdminService, fb: FormBuilder) {
-    const decimal = '[0-9]+(\.?[0-9]+)?'
+  constructor(private admin: AdminService, fb: FormBuilder, private data: DataService) {
+    const decimal = '[0-9]+(\.|,?[0-9]+)?'
     const nodecimal = '[1-9]?[0-9]+'
     this.form = fb.group({
       name: fb.control('', [Validators.required]),
@@ -24,21 +25,26 @@ export class AdditemComponent implements OnInit {
       type: fb.control('', Validators.required),
       count: fb.control('', [Validators.required, Validators.pattern(nodecimal)]),
       image: fb.control('', [Validators.required, Validators.pattern('[a-z0-9]+(_[a-z0-9]+)*')]),
-      details: fb.group({
-        size: fb.control('', [Validators.required]),
-        calories: fb.control('', [Validators.required, Validators.pattern(nodecimal)]),
-        sugarcontent: fb.control('', [Validators.required, Validators.pattern(decimal)]),
-        saltcontent: fb.control('', [Validators.required, Validators.pattern(decimal)]),
-        allergens: fb.control('')
-      })
+      size: fb.control('', [Validators.required]),
+      calories: fb.control('', [Validators.required, Validators.pattern(nodecimal)]),
+      sugarcontent: fb.control('', [Validators.required, Validators.pattern(decimal)]),
+      saltcontent: fb.control('', [Validators.required, Validators.pattern(decimal)]),
+      allergens: fb.control('')
     })
 
-    
+        
+
+
   }
 
   ngOnInit() {
   }
 
+  submit(){
+
+  }
+
+  
 
 
 }
